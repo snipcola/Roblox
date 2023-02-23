ed44a7e06817f8668b6f9afaa0ce932659aa8694
Snipcola <63375371+snipcola@users.noreply.github.com>
2023-02-23 09:56:09 +0000

    Create controller-chat-commands.lua

---
+++ controller-chat-commands.lua
@@ -0,0 +1,40 @@
+local Commands = loadstring(game:HttpGet('https://raw.githubusercontent.com/snipcola/roblox/main/controller-commands.lua'))()
+
+-- Chat System
+local System = {}
+
+System.Config = getgenv().ChatConfig or {
+    Prefix = '.'
+}
+
+-- Dependencies
+local Players = game:GetService('Players')
+local LocalPlayer = Players.LocalPlayer
+
+-- Functions
+function System.SetPrefix(Prefix)
+    System.Config.Prefix = Prefix
+end
+
+function System.OnMessage (Message)
+    local Prefix = string.sub(Message, 1, 1)
+
+    if System.Config.Prefix ~= Prefix then
+        return
+    end
+
+    local Words = Message:split(' ')
+    local Name = Words[1]:sub(2)
+    
+    local Arguments = Words
+
+    table.remove(Arguments, 1)
+
+    Commands.ExecuteCommand(Name, Arguments)
+end
+
+-- Add Chat Event
+if Commands.IsHost() and not getgenv().CommandEventsHooked then
+    LocalPlayer.Chatted:Connect(System.OnMessage)
+    getgenv().CommandEventsHooked = true
+end
